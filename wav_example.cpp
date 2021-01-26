b9d33d531adb3b7272d8cb1a7611e54db91b2c2c
svana00 <54863023+svana00@users.noreply.github.com>
2021-01-26 19:32:22 +0000

    hardcoded stuff

--- wav_example.cpp
+++ wav_example.cpp
@@ -20,12 +20,15 @@ void makeHeader(int sampleRate, int noChannels, int bitsSample)
     header[2] = 'F';
     header[3] = 'F';
 
+    // Chunk size
     int chunkSize = (4 + 8 + 16 + 8 + 0);
-    header[7] = (unsigned char)(((unsigned int)chunkSize & 0xFF000000) >> 24);
-    header[6] = (unsigned char)(((unsigned int)chunkSize & 0x00FF0000) >> 16);
-    header[5] = (unsigned char)(((unsigned int)chunkSize & 0x0000FF00) >> 8);
-    header[4] = (unsigned char)(((unsigned int)chunkSize & 0x000000FF));
 
+    header[4] = (unsigned char)(((unsigned int)chunkSize & 0x000000FF));
+    header[5] = (unsigned char)(((unsigned int)chunkSize & 0x0000FF00) >> 8);
+    header[6] = (unsigned char)(((unsigned int)chunkSize & 0x00FF0000) >> 16);
+    header[7] = (unsigned char)(((unsigned int)chunkSize & 0xFF000000) >> 24);
+
+    // Format
     header[8] = 'W';
     header[9] = 'A';
     header[10] = 'V';
@@ -33,6 +36,52 @@ void makeHeader(int sampleRate, int noChannels, int bitsSample)
     header[12] = 'f';
     header[13] = 'm';
     header[14] = ' ';
+
+    // Subchunk 1 size
+    int subChunk1Size = (4 + 8 + 16 + 8 + 0);
+    header[16] = (unsigned char)(((unsigned int)subChunk1Size & 0x000000FF));
+    header[17] = (unsigned char)(((unsigned int)subChunk1Size & 0x0000FF00) >> 8);
+    header[18] = (unsigned char)(((unsigned int)subChunk1Size & 0x00FF0000) >> 16);
+    header[19] = (unsigned char)(((unsigned int)subChunk1Size & 0xFF000000) >> 24);
+
+    // Audio format
+    header[20] = (unsigned char)(0x01);
+    header[21] = (unsigned char)(0x00);
+
+    // Number of channels
+    header[22] = (unsigned char)(0x01);
+    header[23] = (unsigned char)(0x00);
+
+    // Sample rate
+    header[24] = (unsigned char)(((unsigned int)sampleRate & 0x000000FF));
+    header[25] = (unsigned char)(((unsigned int)sampleRate & 0x0000FF00) >> 8);
+    header[26] = (unsigned char)(((unsigned int)sampleRate & 0x00FF0000) >> 16);
+    header[27] = (unsigned char)(((unsigned int)sampleRate & 0xFF000000) >> 24);
+
+    // Byte rate
+    header[28] = (unsigned char)(((unsigned int)byteRate & 0x000000FF));
+    header[29] = (unsigned char)(((unsigned int)byteRate & 0x0000FF00) >> 8);
+    header[30] = (unsigned char)(((unsigned int)byteRate & 0x00FF0000) >> 16);
+    header[31] = (unsigned char)(((unsigned int)byteRate & 0xFF000000) >> 24);
+
+    // Block align
+    header[32] = (unsigned char)(((unsigned int)blockAlign & 0x000000FF));
+    header[33] = (unsigned char)(((unsigned int)blockAlign & 0x0000FF00) >> 8);
+
+    // Bits per sample
+    header[32] = (unsigned char)(((unsigned int)bitsSample & 0x000000FF));
+    header[33] = (unsigned char)(((unsigned int)bitsSample & 0x0000FF00) >> 8);
+
+    header[34] = 'd';
+    header[35] = 'a';
+    header[36] = 't';
+    header[37] = 'a';
+
+    // Byte rate
+    header[38] = (unsigned char)(((unsigned int)byteRate & 0x000000FF));
+    header[39] = (unsigned char)(((unsigned int)byteRate & 0x0000FF00) >> 8);
+    header[40] = (unsigned char)(((unsigned int)byteRate & 0x00FF0000) >> 16);
+    header[41] = (unsigned char)(((unsigned int)byteRate & 0xFF000000) >> 24);
 }
 
 void addDataToArray()
