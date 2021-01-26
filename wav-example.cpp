d34c7fac3f62f56e2db446f602c460a18244135f
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2021-01-26 18:30:35 +0000

    Added RIFF to header array

--- wav-example.cpp
+++ wav-example.cpp
@@ -2,13 +2,23 @@
 
 using namespace std;
 
+// Initialize global variables for header array and data array
 char header[44];
 char data[44100];
 
 /* Add two functions here */
-void makeHeader()
+void makeHeader(int sampleRate, int noChannels, int bitsSample)
 {
+    int byteRate = sampleRate * noChannels * bitsSample/8;
+    int blockAlign = noChannels * bitsSample/8;
 
+    int int_field = 0;
+
+    // Set ChunkID at first 4 bytes
+    header[0] = 'R';
+    header[1] = 'I';
+    header[2] = 'F';
+    header[3] = 'F';
 }
 
 void addDataToArray()
