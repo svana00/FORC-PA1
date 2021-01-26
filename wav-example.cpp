fca075e7e0ebd1d7028f76b11424b26ed87a5c3d
bjorgvin16 <54938489+bjorgvin16@users.noreply.github.com>
2021-01-26 17:40:58 +0000

    made structure

--- wav-example.cpp
+++ wav-example.cpp
@@ -2,10 +2,28 @@
 
 using namespace std;
 
+char header[44];
+char data[44100];
+
 /* Add two functions here */
+void makeHeader()
+{
+
+}
+
+void addDataToArray()
+{
+
+}
 
 int main()
 {
+    int sampleRate = 44100;
+    int freq = 440;
+    double duration = 0.5;
+    int noChannels = 1;
 
+    int noSamples;
+    noSamples = duration * sampleRate;
     return 0;
 }
