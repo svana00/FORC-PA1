d949ec06ab04c123fa85a7adeb8bd18ba97a04c7
svana00 <54863023+svana00@users.noreply.github.com>
2021-01-26 18:21:01 +0000

    cmts

--- wav-example.cpp
+++ wav-example.cpp
@@ -8,22 +8,20 @@ char data[44100];
 /* Add two functions here */
 void makeHeader()
 {
-
 }
 
 void addDataToArray()
 {
-
 }
 
 int main()
 {
-    int sampleRate = 44100;
-    int freq = 440;
-    double duration = 0.5;
-    int noChannels = 1;
+    int sampleRate = 44100; // Sample rate in HZ
+    int freq = 440;         // A above middle C
+    double duration = 0.5;  // Length of tone - seconds
+    int noChannels = 1;     // Mono
 
     int noSamples;
-    noSamples = duration * sampleRate;
+    noSamples = duration * sampleRate; // Total number of samples for file
     return 0;
 }
